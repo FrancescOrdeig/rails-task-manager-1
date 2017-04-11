46ff9c0ef3f3b61f4a5a68bbacdeb1800724ca80
Arthur Ramos <ramos.oceano@gmail.com>
2017-04-11 14:56:50 -0300

    tasks exercise

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411142537) do
+ActiveRecord::Schema.define(version: 20170411151312) do
 
   create_table "tasks", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "name"
+    t.string   "description"
   end
 
 end
